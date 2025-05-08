03941f0bed7e5703192d561e67442aaf5fc4f3c9
yu wong <sakuragi.yu@gmail.com>
2025-05-08 17:56:56 +0100

    after live-code

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,14 +1,14 @@
 class BookmarksController < ApplicationController
-  before_action :set_bookmark, only: [:destroy]
-  before_action :set_list, only: [:create]
-
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
+
     if @bookmark.save
       redirect_to list_path(@list)
     else
@@ -17,6 +17,7 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
     if @bookmark.destroy
       redirect_to list_path(@bookmark.list), status: :see_other
     else
@@ -28,13 +29,5 @@ class BookmarksController < ApplicationController
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
-  end
-
-  def set_bookmark
-    @bookmark = Bookmark.find(params[:id])
-  end
-
-  def set_list
-    @list = List.find(params[:list_id])
   end
 end
